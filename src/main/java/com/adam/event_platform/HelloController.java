61860065c6659ee962034a69d7ed78ce65817e7e
AdamKatrenic <adamkatrenic@gmail.com>
2026-07-16 18:33:06 +0200

    testing local ai

--- src/main/java/com/adam/event_platform/HelloController.java
+++ src/main/java/com/adam/event_platform/HelloController.java
@@ -1,4 +1,18 @@
 package com.adam.event_platform;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Welcome to the Event Platform!";
+    }
+
+    @GetMapping("/smiley")
+    public String getSmileyFace() {
+        return "<div style='font-size: 48px;'></div><p>The platform is running smoothly!</p>";
+    }
 }
